e5e79eeea249297db9827c2172134a17278f3cb8
Ryan Owl <ryan.owl@temple.edu>
2023-06-29 12:33:56 -0400

    test for insertion

--- create_DB_and_tables.py
+++ create_DB_and_tables.py
@@ -1,4 +1,6 @@
+from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.orm import Session
 
 def engine_creation(source_db_uri, target_db_uri):
     source_db_engine = create_engine(source_db_uri, echo = True)
@@ -14,12 +16,19 @@ def meta_reflection(source_db_engine, target_db_engine):
 
     return source_db_meta, target_db_meta
 
+def automap_classes(engine):
+    Base = automap_base()
+    Base.prepare(autoload_with=engine)
+
+    return Base
+
+
 def meta_creation():
     meta = MetaData()
 
     students = Table(
       'students', meta, 
-      Column('id', Integer, primary_key = True), 
+      Column('id', Integer, primary_key = True, autoincrement=True), 
       Column('name', String), 
       Column('lastname', String),
     )
@@ -52,3 +61,33 @@ def drop_all_tables(source_db_engine, target_db_engine, meta):
         meta.drop_all(target_db_engine)
     except Exception as e:
         print(e)
+
+def insert_test_data_source_db(engine):
+    Base = automap_classes(engine)
+    students = Base.classes.students
+    student1 = students(name = "leefun owl", lastname = "owl")
+    student2 = students(name = "nick foles", lastname = "foles")
+    student3 = students(name = "tom brady", lastname = "brady")
+    
+    with Session(engine) as session:
+        session.add(student1)
+        session.add(student2)
+        session.add(student3)
+        session.commit()
+        
+        return session.query(students).count()
+
+def insert_test_data_target_db(engine):
+    Base = automap_classes(engine)
+    students = Base.classes.students
+    student1 = students(name = "leefun owl", lastname = "owl")
+    student2 = students(name = "foles", lastname = "foles")
+    student3 = students(name = "tom", lastname = "brady")
+    
+    with Session(engine) as session:
+        session.add(student1)
+        session.add(student2)
+        session.add(student3)
+        session.commit()
+        
+        return session.query(students).count()
--- test/test_schema_creation.py
+++ test/test_schema_creation.py
@@ -1,6 +1,6 @@
 import unittest
 
-from create_DB_and_tables import schema_creation, drop_all_tables, engine_creation, meta_creation, meta_reflection
+from create_DB_and_tables import *
 from config import test as config
 
 class TestSchema(unittest.TestCase):
@@ -12,22 +12,27 @@ class TestSchema(unittest.TestCase):
         self.target_db_engine = target_db_engine
         self.meta = meta
 
-    def test_schema_deletion(self):
-        # source_db_engine, target_db_engine = engine_creation(config.source_db_uri, config.target_db_uri)
-        # meta = meta_creation()
-        # drop_all_tables(source_db_engine, target_db_engine, meta)
-        # source_db_meta, target_db_meta = meta_reflection(source_db_engine, target_db_engine)
-        drop_all_tables(self.source_db_engine, self.target_db_engine, self.meta)
-        source_db_meta, target_db_meta = meta_reflection(self.source_db_engine, self.target_db_engine)
-        # Confirm no tables in DBs
-        self.assertEqual(len(source_db_meta.tables.keys()), 0)
-        self.assertEqual(len(target_db_meta.tables.keys()), 0)
-
-
-    def test_schema_creation(self):
         drop_all_tables(self.source_db_engine, self.target_db_engine, self.meta)
         schema_creation(self.source_db_engine, self.target_db_engine, self.meta)
 
+    @unittest.skip("testing insert")
+    def test_schema_deletion(self):
+        drop_all_tables(self.source_db_engine, self.target_db_engine, self.meta)
+        source_db_meta, target_db_meta = meta_reflection(self.source_db_engine, self.target_db_engine)
+        self.assertEqual(len(source_db_meta.tables.keys()), 0)
+        self.assertEqual(len(target_db_meta.tables.keys()), 0)
+        schema_creation(self.source_db_engine, self.target_db_engine, self.meta)
+
+    @unittest.skip("testing insert")
+    def test_schema_creation(self):
+        drop_all_tables(self.source_db_engine, self.target_db_engine, self.meta)
+        schema_creation(self.source_db_engine, self.target_db_engine, self.meta)
         source_db_meta, target_db_meta = meta_reflection(self.source_db_engine, self.target_db_engine)
         self.assertNotEqual(len(source_db_meta.tables.keys()), 0)
         self.assertNotEqual(len(target_db_meta.tables.keys()), 0)
+
+    def test_insert(self):
+        count_target_db = insert_test_data_target_db(self.target_db_engine)
+        count_source_db = insert_test_data_source_db(self.source_db_engine)
+        self.assertNotEqual(count_target_db, 0)
+        self.assertNotEqual(count_source_db, 0)
